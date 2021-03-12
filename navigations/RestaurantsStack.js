b8f6a486c34bffd794c7def3496d2a89371529c2
duqueg24 <78827687+duqueg24@users.noreply.github.com>
2021-03-12 16:12:52 -0500

    Add navigation

---
+++ navigations/RestaurantsStack.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack'
+import Restaurants from '../screens/Restaurants'
+
+const Stack = createStackNavigator()
+
+
+export default function RestaurantsStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+            name="restaurantes"
+            component={Restaurants}
+            options={{title:"Restaurantes"}}
+            
+            />
+        </Stack.Navigator>
+    )
+}
